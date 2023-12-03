eeaa58312aed8d5300cf9e284944d788a304ab2e
your name <eli.pousson@gmail.com>
2023-12-02 23:35:18 -0500

    Add tests for `qto_block()` + `qto_div()`

---
+++ tests/testthat/_snaps/block.md
@@ -0,0 +1,11 @@
+# qto_div works
+
+    Code
+      qto_div("Example text.", id = "id", class = "class")
+    Output
+      
+      ::: {#id .class}
+      Example text. 
+      :::
+      
+
---
+++ tests/testthat/test-block.R
@@ -0,0 +1,9 @@
+test_that("qto_block works", {
+  expect_s3_class(qto_block(), "quarto_block")
+  expect_type(qto_block(), "character")
+  expect_length(qto_block(), 1)
+})
+
+test_that("qto_div works", {
+  expect_snapshot(qto_div("Example text.", id = "id", class = "class"))
+})
