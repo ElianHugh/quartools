7bfb17a4aa24f88197094512043ddc528428d411
your name <eli.pousson@gmail.com>
2023-12-02 22:55:12 -0500

    Add `qto_heading()` and `qto_hr()`
    `qto_heading()` is required for  `qto_callout()`

---
+++ R/basics.R
@@ -0,0 +1,32 @@
+#' Basic formatting and markdown elements
+#'
+#' [qto_heading()] creates headings and [qto_hr()] creates horizontal rules.
+#'
+#' @name qto_basics
+NULL
+
+#' @rdname qto_basics
+#' @name qto_heading
+#' @param ... Heading text passed to [qto_block()].
+#' @param level Heading level. Defaults to 1.
+#' @export
+qto_heading <- function(...,
+                        level = 1) {
+  qto_block(strrep("#", level), " ", ...)
+}
+
+#' @rdname qto_basics
+#' @name qto_heading
+#' @param rule,length Horizontal rule character and length of rule.
+#' @param before,after Text to insert before and after a horizontal rule.
+#' @export
+qto_hr <- function(rule = "-",
+                   length = 72,
+                   before = "\n\n",
+                   after = before) {
+  qto_block(
+    before,
+    strrep(rule, length),
+    after
+  )
+}
