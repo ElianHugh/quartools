3716745b5290588fdb350a2eb73be6b459182e1c
your name <eli.pousson@gmail.com>
2023-12-07 22:31:40 -0500

    Add ordered (qto_ol) + unordered lists (qto_li)
    Also rename qto_definition_list to qto_dl

--- R/lists.R
+++ R/lists.R
@@ -1,5 +1,8 @@
 #' Create a definition list
 #'
+#'  See the Quarto documentation for more information on lists:
+#' <https://quarto.org/docs/authoring/markdown-basics.html#lists>
+#'
 #' @param ... Optional. Named argument where the argument name is a term and the
 #'   value name is the text to display as a definition for the term. Ignored if
 #'   `.list` or `.term` and `.definition` are supplied.
@@ -8,14 +11,18 @@
 #' @param .list If `.list` is supplied, any values passed to `...` are ignored.
 #'   One of `.list`, `.term` and `.definition`, or `...` must be supplied.
 #' @inheritParams list_drop_or_replace_na
+#' @aliases qto_definition_list
+#' @seealso
+#' - [qto_ol()]
+#' - [qto_li()]
 #' @export
-qto_definition_list <- function(...,
-                                .term = NULL,
-                                .definition = NULL,
-                                .list = NULL,
-                                .drop_na = FALSE,
-                                .sep = "\n",
-                                .replacement = "") {
+qto_dl <- function(...,
+                   .term = NULL,
+                   .definition = NULL,
+                   .list = NULL,
+                   .drop_na = FALSE,
+                   .sep = "\n",
+                   .replacement = "") {
   if (!is.null(.term) && !is.null(.definition)) {
     .list <- .list %||% set_names(.definition, .term)
   }
@@ -38,4 +45,70 @@ qto_definition_list <- function(...,
     .list,
     collapse = "\n\n"
   )
+}
+
+#' Create ordered or unordered lists for Quarto
+#'
+#' [qt_ol()] created ordered lists and [qt_li()] creates unordered lists. See
+#' the Quarto documentation for more information on lists:
+#' <https://quarto.org/docs/authoring/markdown-basics.html#lists>
+#'
+#' @param .list A vector of list items. Optional if items are passed to `...`.
+#' @param ... Items to use as list items.
+#' @param level Indent level of list. Nested levels in a single function call
+#'   are not yet supported.
+#' @seealso [qto_dl()]
+#' @examples
+#' qto_ol(LETTERS[1:3])
+#'
+#' qto_li(LETTERS[4:6])
+#'
+#' qto_li(LETTERS[7:9], level = 2)
+#'
+#' @name qto_list
+NULL
+
+#' @rdname qto_list
+#' @name qto_ol
+#' @export
+qto_ol <- function(.list = NULL, ..., level = 1) {
+  qto_li(
+    .list = .list,
+    ...,
+    level = level,
+    symbol = "1."
+  )
+}
+
+#' @rdname qto_list
+#' @name qto_li
+#' @param symbol Symbol to use for list bullet. Use "(@)" to create a list where
+#'   numbering continues after interruption.
+#' @export
+qto_li <- function(.list = NULL, ..., level = 1, symbol = "*") {
+  .list <- .list %||% rlang::list2(...)
+
+  check_string(symbol, allow_empty = FALSE)
+
+  qto_block(
+    "",
+    paste0(
+      indent_level(level),
+      symbol, " ",
+      .list
+    ),
+    collapse = "\n"
+  )
+}
+
+#' Helper function to set indent based on level
+#'
+#' @noRd
+indent_level <- function(level = 1) {
+  if (level == 1) {
+    return("")
+  }
+
+  level <- level - 1
+  strrep(" ", 4 * level)
 }
